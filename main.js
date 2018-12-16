2ac12166a79ee13926984ff8a034f256b35915a4
William Leivo <william@cetrez.com>
2018-12-16 21:22:35 +0100

    Finished the Screeps Tutorial

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
-
 module.exports.loop = function () {
+
+  var tower = Game.getObjectById('351192aa80bc7d37bd05136a');
+  if (tower) {
+    var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+      filter: (structure) => structure.hits < structure.hitsMax
+    });
+    if (closestDamagedStructure) {
+      tower.repair(closestDamagedStructure);
+    }
+
+    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if (closestHostile) {
+      tower.attack(closestHostile);
+    }
+  }
 
   var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
   console.log(`Harvesters: ${harvesters.length}`);
-
   for (var name in Memory.creeps) {
     if (!Game.creeps[name]) {
       delete Memory.creeps[name];
       console.log('Clearing non-existing creep memory:', name);
     }
   }
-
     if (harvesters.length < 2) {
       var newName = 'Harvester' + Game.time;
       console.log(`Spawning new harvester: ${newName}`);
       Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName,
         { memory: { role: 'harvester' } });
     }
-
     if (Game.spawns['Spawn1'].spawning) {
       var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
       Game.spawns['Spawn1'].room.visual.text(
@@ -29,23 +40,16 @@ module.exports.loop = function () {
         Game.spawns['Spawn1'].pos.y,
         { align: 'left', opacity: 0.8 });
     }
-
   for (var name in Game.creeps) {
-
     var creep = Game.creeps[name];
-  
     if (creep.memory.role == 'harvester') {
       roleHarvester.run(creep);
     }
-
     if (creep.memory.role == 'upgrader') {
       roleUpgrader.run(creep);
     }
-
     if (creep.memory.role == 'builder') {
       roleBuilder.run(creep);
     }
-
   }
-
 }
