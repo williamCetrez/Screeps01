0e214ad7b194af8b022fd665185d2fbd7688d276
William Leivo <william@cetrez.com>
2018-12-16 20:50:01 +0100

    Finished Screeps tutorial section 3

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
 module.exports.loop = function () {
   for (var name in Game.creeps) {
     var creep = Game.creeps[name];
@@ -8,6 +9,9 @@ module.exports.loop = function () {
     }
     if (creep.memory.role == 'upgrader') {
       roleUpgrader.run(creep);
+    }
+    if (creep.memory.role == 'builder') {
+      roleBuilder.run(creep);
     }
   }
 }
